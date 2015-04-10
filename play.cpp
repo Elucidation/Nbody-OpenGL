6e2c9eb4af002484898f80538886955a4fce48c4
Sameer Ansari <elucidation@gmail.com>
2015-04-10 04:06:54 -0700

    updated readme
    spinning triangle

--- play.cpp
+++ play.cpp
@@ -1,8 +1,97 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <glm/glm.hpp>
+using namespace glm;
+
+
+void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+GLFWwindow* setupGL()
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+
+    // Need OpenGL3.3 so not on this laptop
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
+
+    window = glfwCreateWindow(640, 480, "I am Sam", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    return window;
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("Hi there again\n");
-    return 0;
+    // Setup
+     GLFWwindow* window = setupGL();
+
+    // The meat
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    
+    glfwSetKeyCallback(window, key_callback);
+
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Keep running
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        double gTime = glfwGetTime();
+        printf("Hi there again at time %g\n", gTime);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Wrap up
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }   
